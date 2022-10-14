772a30f14d6ba4aa09a88e1c886fe2d8868814b2
wazzywael <wael.k@hotmail.co.uk>
2022-10-15 00:21:01 +0100

    Todo project initial commit

---
+++ src/Todo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Todo = ({ todo, toggleTodo }) => {
+  const handleTodoClick = () => {
+    toggleTodo(todo.id);
+  };
+
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={todo.complete}
+        onChange={handleTodoClick}
+      />
+      {todo.name}
+    </label>
+  );
+};
+
+export default Todo;
